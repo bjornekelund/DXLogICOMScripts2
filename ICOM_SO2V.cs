b8b52ff437d9b7a856a3bfa1a65ac4ed3d6840b0
Björn Ekelund <bjorn@ekelund.nu>
2019-07-30 11:31:51 +0200

    Corrections

--- ICOM_SO2V.cs
+++ ICOM_SO2V.cs
@@ -1,13 +1,13 @@
 //INCLUDE_ASSEMBLY System.dll
 //INCLUDE_ASSEMBLY System.Windows.Forms.dll
 
-// ICOM SO2V VFO focus and audio management, event driven but also mapped 
-// to key for temporary stereo on/off toggling.
+// ICOM SO2V VFO focus and audio management for Microham MK2R/u2R, 
+// event driven but also mapped to key for temporary stereo on/off toggling.
 // Key used is typically § (on EU keyboard) or `(on US keyboard) 
 // to maintain muscle-memory compatibility with N1MM.
 // Tested on IC-7610 but should work on all modern dual receiver radios.
 // Use Ctrl-Alt-S/AltGr-S to toggle permanent stereo on/off. 
-// By Björn Ekelund SM7IUN sm7iun@ssa.se 2019-07-28
+// By Björn Ekelund SM7IUN sm7iun@ssa.se 2019-07-30
 
 using IOComm;
 
@@ -15,11 +15,8 @@ namespace DXLog.net
 {
     public class IcomSO2V : ScriptClass
     {
-        readonly bool Debug = false;
-        ContestData cdata;
         FrmMain mainForm;
         COMPort microHamPort;
-
 
         readonly byte[] IcomDualWatchOn = { 0x07, 0xC1 };
         readonly byte[] IcomSelectMain = { 0x07, 0xD0 };
@@ -27,36 +24,31 @@ namespace DXLog.net
         readonly byte[] IcomSplitOff = { 0x0F, 0x00 };
         readonly string statusMessage = "Focus on {0} VFO. {1}.";
 
-        bool tempStereoAudio;
-        int lastFocus;
+        int lastFocusedRadio;
+        bool tempStereoToggle;
 
         // Executes at DXLog.net start 
 
         public void Initialize(FrmMain main)
         {
-            CATCommon radio1 = main.COMMainProvider.RadioObject(1);
-            cdata = main.ContestDataProvider;
             mainForm = main;
+            CATCommon radio1 = main.COMMainProvider.RadioObject(1);
 
             // Find Microham port
             foreach (COMPort _comport in main.COMMainProvider._com)
-            {
-                if (_comport != null || _comport._portDeviceName == "MK2R/MK2R+/u2R")
-                {
-                    if (_comport._mk2r != null)
-                    {
-                        microHamPort = _comport;
-                        //mainForm.SetMainStatusText("Found Microham device");
-                    }
-                }
-            }
+                if (_comport._mk2r != null)
+                    microHamPort = _comport;
+
+            if (microHamPort == null)
+                main.COMMainProvider.SignalMessage("ICOM_SO2V: ERROR no Microham device");
 
             // Initialize temporary stereo mode to DXLog's stereo mode to support temporary toggle
-            // At start up, radio 1 is always focused and stereo audio is disabled
-            tempStereoAudio = mainForm.ListenStatusMode == 3;
-            lastFocus = 1;
+            // At start up, radio 1 is always focused and stereo audio is forced
+            main.SetListenStatusMode(3, true, false);
+            tempStereoToggle = false;
+            lastFocusedRadio = 1;
 
-            cdata.FocusedRadioChanged += new ContestData.FocusedRadioChange(HandleFocusChange);
+            main.ContestDataProvider.FocusedRadioChanged += new ContestData.FocusedRadioChange(HandleFocusChange);
 
             // Only initialize radio if present and ICOM
             if (radio1 != null)
@@ -67,8 +59,6 @@ namespace DXLog.net
                     radio1.SendCustomCommand(IcomSelectMain);
                     radio1.SendCustomCommand(IcomSplitOff);
                 }
-
-            main.SetListenStatusMode(3, false, false);
         }
 
         public void Deinitialize() { }
@@ -78,10 +68,12 @@ namespace DXLog.net
         public void Main(FrmMain main, ContestData cdata, COMMain comMain)
         {
             int focusedRadio = cdata.FocusedRadio;
+            bool stereoAudio = main.ListenStatusMode == 3;
+            bool tempStereoAudio; 
 
             if (cdata.OPTechnique == ContestData.Technique.SO2V)
             {
-                tempStereoAudio = !tempStereoAudio;
+                tempStereoAudio = tempStereoToggle ? stereoAudio : !stereoAudio;
 
                 if (microHamPort != null)
                 {
@@ -90,12 +82,9 @@ namespace DXLog.net
                     if (tempStereoAudio)
                         microHamPort._mk2r.SendCustomCommand("FRS");
                     else
-                    {
-                        if (focusedRadio == 1)
-                            microHamPort._mk2r.SendCustomCommand("FR1");
-                        else
-                            microHamPort._mk2r.SendCustomCommand("FR2");
-                    }
+                        microHamPort._mk2r.SendCustomCommand(string.Format("FR{0}", focusedRadio == 1 ? 1 : 2));
+
+                    tempStereoToggle = !tempStereoToggle;
                 }
             }
         }
@@ -105,46 +94,28 @@ namespace DXLog.net
         private void HandleFocusChange()
         {
             CATCommon radio1 = mainForm.COMMainProvider.RadioObject(1);
-            int focusedRadio = cdata.FocusedRadio;
+            int focusedRadio = mainForm.ContestDataProvider.FocusedRadio;
             // ListenStatusMode: 0=Radio 1, 1=Radio 2 toggle, 2=Radio 2, 3=Both
-            int listenMode = mainForm.ListenStatusMode;
-            bool stereoAudio = listenMode == 3;
-            bool modeIsSo2V = cdata.OPTechnique == ContestData.Technique.SO2V;
-            string audioStatus;
+            bool stereoAudio = mainForm.ListenStatusMode == 3;
+            bool modeIsSo2V = mainForm.ContestDataProvider.OPTechnique == ContestData.Technique.SO2V;
 
-            if (modeIsSo2V && focusedRadio != lastFocus) // Only active in SO2V and with ICOM. Ignore false triggers.
+            tempStereoToggle = false;
+
+            if (modeIsSo2V && focusedRadio != lastFocusedRadio) // Only active in SO2V. Ignore redundant invokations.
             {
-                tempStereoAudio = stereoAudio; // Set temporary stereo mode to DXLog's stereo mode to support temporary toggle
-                lastFocus = focusedRadio;
+                lastFocusedRadio = focusedRadio;
 
                 if (microHamPort != null)
-                {
                     if (stereoAudio)
                         mainForm.SetListenStatusMode(3, true, false);
                     else
-                    {
-                        if (focusedRadio == 1)
-                        {
-                            mainForm.SetListenStatusMode(0, false, false); // To set ListenStatusMode correctly
-                            microHamPort._mk2r.SendCustomCommand("FR1");  // Override, to select correct radio in Microham
-                        }
-                        else
-                        {
-                            mainForm.SetListenStatusMode(0, false, false); // To set ListenStatusMode correctly
-                            microHamPort._mk2r.SendCustomCommand("FR2");  // Override, to select correct radio in Microham
-                        }
-                    }
-                }
+                        mainForm.SetListenStatusMode(0, false, false);
 
                 if (radio1 != null)
                     if (radio1.IsICOM())
                     radio1.SendCustomCommand(focusedRadio == 1 ? IcomSelectMain : IcomSelectSub);
 
-                audioStatus = stereoAudio ? "Stereo" : "Single receiver";
-                if (Debug) mainForm.SetMainStatusText(string.Format("IcomSO2V: Listenmode {0}. Focus is Radio #{1}, {2}.",
-                    listenMode, focusedRadio, audioStatus));
-                else
-                    mainForm.SetMainStatusText(string.Format(statusMessage, focusedRadio == 1 ? "Main" : "Sub", audioStatus));
+                mainForm.SetMainStatusText(string.Format(statusMessage, focusedRadio == 1 ? "Main" : "Sub", stereoAudio ? "Stereo" : "Single receiver"));
             }
         }
     }
--- ICOM_SO2V_STEREO.cs
+++ ICOM_SO2V_STEREO.cs
@@ -1,9 +1,10 @@
 //INCLUDE_ASSEMBLY System.dll
 //INCLUDE_ASSEMBLY System.Windows.Forms.dll
 
-// ICOM SO2V helper script to toggle permanent stereo on and off. 
-// Mapped to Ctrl-Alt-S (AltGr-S). 
-// By Björn Ekelund SM7IUN sm7iun@ssa.se 2019-07-29
+// ICOM SO2V helper script to toggle permanent stereo on 
+// and off with Microham MK2R/u2R.
+// Replaces DXLog's own Ctrl-Alt-S (AltGr-S) key.
+// By Björn Ekelund SM7IUN sm7iun@ssa.se 2019-07-30
 
 using IOComm;
 
@@ -11,29 +12,9 @@ namespace DXLog.net
 {
     public class IcomSO2Stereo : ScriptClass
     {
-        readonly bool Debug = false;
-
         readonly string statusMessage = "Focus on {0} VFO. {1}.";
-        COMPort microHamPort = null;
 
-
-        // Executes at DXLog.net start 
-        public void Initialize(FrmMain main)
-        {
-            // Find and save Microham port
-            foreach (COMPort _comport in main.COMMainProvider._com)
-            {
-                if (_comport != null || _comport._portDeviceName == "MK2R/MK2R+/u2R")
-                {
-                    if (_comport._mk2r != null)
-                    {
-                        microHamPort = _comport;
-                        //main.SetMainStatusText("Found Microham device");
-                    }
-                }
-            }
-        }
-
+        public void Initialize(FrmMain main) { }
         public void Deinitialize() { } 
 
         // Toggle permanent stereo, execution of Main is mapped to Ctrl-Alt-S
@@ -44,27 +25,17 @@ namespace DXLog.net
             bool stereoAudio = main.ListenStatusMode == 3;
             bool modeIsSo2V = cdata.OPTechnique == ContestData.Technique.SO2V;
 
-            if (modeIsSo2V && microHamPort != null)
+            if (modeIsSo2V)
             {
                 if (!stereoAudio)
-                    main.SetListenStatusMode(3, false, false);
+                    main.SetListenStatusMode(3, true, false);
                 else
-                {
-                    if (focusedRadio == 1)
-                    {
-                        main.SetListenStatusMode(0, false, false); // To set ListenStatusMode correctly
-                        microHamPort._mk2r.SendCustomCommand("FR1");  // Override, to select correct radio in Microham
-                    }
-                    else
-                    {
-                        main.SetListenStatusMode(0, false, false); // To set ListenStatusMode correctly
-                        microHamPort._mk2r.SendCustomCommand("FR2");  // Override, to select correct radio in Microham
-                    }
-                }
+                    main.SetListenStatusMode(0, true, false);
+
                 main.SetMainStatusText(string.Format(statusMessage, focusedRadio == 1 ? "Main" : "Sub", stereoAudio ? "Single receiver" : "Stereo"));
             }
 
-            main.ScriptContinue = false;
+            main.ScriptContinue = false; // Do not continue with DXLog's own key definition
         }
     }
 }
