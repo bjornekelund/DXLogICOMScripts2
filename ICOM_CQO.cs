ee9d587d9836e9f1a4e27ff0936e33e5ce3afb38
Björn Ekelund <bjorn@ekelund.nu>
2020-03-23 23:40:06 +0100

    Added script for CQ on other radio

--- ICOM_CQO.cs
+++ ICOM_CQO.cs
@@ -1,10 +1,8 @@
 //INCLUDE_ASSEMBLY System.dll
 //INCLUDE_ASSEMBLY System.Windows.Forms.dll
 
-// ICOM SO2V helper script to toggle permanent stereo on 
-// and off with Microham MK2R/u2R.
-// Replaces DXLog's own Ctrl-Alt-S (AltGr-S) key.
-// By Björn Ekelund SM7IUN sm7iun@ssa.se 2019-07-30
+// ICOM_CQO script to switch to other radio and transmit CQ
+// By Björn Ekelund SM7IUN sm7iun@ssa.se 2020-03-23
 
 using IOComm;
 using System.Threading;
@@ -13,53 +11,28 @@ namespace DXLog.net
 {
     public class CqOtherRadio : ScriptClass
     {
-        readonly string statusMessage = "Focus on radio {0}.";
-
         public void Initialize(FrmMain main) { }
+
         public void Deinitialize() { }
 
         // Toggle radio focus, call CQ
         public void Main(FrmMain main, ContestData cdata, COMMain comMain)
         {
-            if (cdata.OPTechnique != ContestData.Technique.SO1R)
+            switch (cdata.OPTechnique)
             {
-                switch (cdata.OPTechnique)
-                {
-                    case ContestData.Technique.SO2R:
-                        if (cdata.FocusedRadio == 1)
-                        {
-                            cdata.ActiveRadio = 2;
-                            cdata.FocusedRadio = 2;
-                        }
-                        else
-                        {
-                            cdata.ActiveRadio = 1;
-                            cdata.FocusedRadio = 1;
-                        }
-                        break;
-                    case ContestData.Technique.SO2R_ADV:
-                    case ContestData.Technique.SO2V:
-                        cdata.FocusedRadio = cdata.FocusedRadio == 1 ? 2 : 1;
-                        break;
-                }
-
-                comMain.SetActiveRadio(cdata.FocusedRadio);
-                cdata.TXOnRadio = cdata.FocusedRadio;
-
-                //UCQSO cl1 = main.CurrentEntryLine;
-                //if (cl1 != null && cl1.ActualQSO != null && cl1.ActualQSO.Callsign == string.Empty)
-                //    cl1.Controls["txtCallSign"].Focus();
-
-//                Thread.Sleep(1000);
-                //main.SendFKeyMessage("F1");
-
-                main.SetMainStatusText(string.Format(statusMessage, cdata.FocusedRadio));
-
-                main.ScriptContinue = false; // Do not continue with DXLog's own key definition
+                case ContestData.Technique.SO2R:
+                case ContestData.Technique.SO2V:
+                    cdata.ActiveRadio = (cdata.ActiveRadio == 1 ? 2 : 1);
+                    cdata.FocusedRadio = cdata.ActiveRadio;
+                    Thread.Sleep(50); // Wait 50ms for switch to safely complete
+                    main.SendKeyMessage(false, "F1");
+                    main.SetMainStatusText(string.Format("Focus on radio {0}.", cdata.ActiveRadio));
+                    main.ScriptContinue = false; // Do not continue with DXLog's own key definition
+                    break;
+                default:
+                    main.ScriptContinue = true; // Use DXLog's own key definition if SO1R or SO2R advanced
+                    break;
             }
-            else
-                main.ScriptContinue = true; // Use DXLog's own key definition if SO1R
-
         }
     }
 }
